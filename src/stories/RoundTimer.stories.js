2239b75d8a5a089e85841be673ec1d3d54c6ce79
ajiop0123@gmail.com <=>
2020-11-26 00:41:38 +0900

    [ Feature ] RoundTimer 추가

---
+++ src/stories/RoundTimer.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import RoundTimer from '@/components/elements/ingame/RoundTimer';
+
+export default {
+  title: 'RoundTimer',
+  component: RoundTimer,
+};
+
+export const Default = () => <RoundTimer />;
