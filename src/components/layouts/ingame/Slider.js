9b901d938e50a44f5b3b6371bffe51aff533ccfa
Jiwon <ajiop0123@gmail.com>
2020-11-29 20:20:51 +0900

    [ Refactor ] Slider 스타일링 수정

--- src/components/layouts/ingame/Slider.js
+++ src/components/layouts/ingame/Slider.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import '@/assets/scss/layouts/ingame/SliderBox.scss';
+import '@/assets/scss/layouts/ingame/Slider.scss';
 // components
 import SliderBox from './SliderBox';
 
 function Slider({ topic }) {
   const topicArray = topic.split('');
   return (
-    <>
+    <div className="slider">
       {topicArray.map(function (wordNow, wordIndex) {
         return (
           <SliderBox topic={topic} wordNow={wordNow} wordIndex={wordIndex} />
         );
       })}
-    </>
+    </div>
   );
 }
 
