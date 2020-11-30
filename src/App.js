df87274499564c7733caa1772f0c6caaa91d9399
MS\kwakm <kwakms821@daum.net>
2020-11-30 22:34:15 +0900

    [ Feat ] Wordbox, Resultbox 기능개발

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+// import Button from './components/commons/Button';
+// import Wordbox from './components/commons/Wordbox';
+import Resultbox from './components/commons/Resultbox';
+// import Wordbox from './components/commons/Wordbox';
 
 const App = () => {
   return (
     <div>
       <h1>Test</h1>
+      {/* <Wordbox topic="하" text="하나둘셋" /> */}
+      <Resultbox topic="주제어" text="7자까지 쓸게요" />
     </div>
   );
 };
--- src/components/commons/Resultbox.js
+++ src/components/commons/Resultbox.js
@@ -1,31 +1,20 @@
 import React from 'react';
+import classNames from 'classnames';
 import '@/assets/scss/commons/Resultbox.scss';
 
-const ResultBox = () => {
+const ResultBox = ({ topic, text, color }) => {
   return (
-    <>
-      <ul className="resultbox-container">
-        <li>
-          <div className="resultbox resultbox-topic">
-            <span className="resultbox-title">주제어</span>
-            <span className="resultbox-body">7자까지 쓸게요</span>
-          </div>
-        </li>
-        <li>
-          <div className="resultbox resultbox-timer">
-            <span className="resultbox-title">소요시간</span>
-            <span className="resultbox-body">00:00</span>
-          </div>
-        </li>
-        <li>
-          <div className="resultbox resultbox-level">
-            <span className="resultbox-title">난이도</span>
-            <span className="resultbox-body">지렁이</span>
-          </div>
-        </li>
-      </ul>
-    </>
+    <div className={classNames('resultbox', color)}>
+      <span>{topic}</span>
+      <span>{text}</span>
+    </div>
   );
 };
 
 export default ResultBox;
+
+ResultBox.defaultProps = {
+  topic: '주제어',
+  text: '7자까지 쓸게요',
+  color: 'black',
+};
