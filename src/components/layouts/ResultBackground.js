9fa11e93cc458a990c40964fac3d5a37e9bb075a
Jiwon Jeon <ajiop0123@gmail.com>
2020-12-06 16:18:43 +0900

    [ Feature ] ResultBackground Layout 추가

---
+++ src/components/layouts/ResultBackground.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import '@/assets/scss/layouts/ResultBackground.scss';
+import classNames from 'classnames';
+import ResultBox from '@/components/commons/ResultBox';
+
+const ResultBackground = ({ success }) => {
+  return (
+    <>
+      {success ? (
+        <div
+          className={classNames(
+            'result-bar',
+            success === true ? 'success' : '',
+          )}
+        >
+          <h2 className="result-bar--title">SUCCESS</h2>
+          <div className="result-bar--desc">
+            <ResultBox color="white" />
+            <ResultBox color="black" />
+            <ResultBox color="gray" />
+          </div>
+        </div>
+      ) : (
+        <div
+          className={classNames(
+            'result-bar',
+            success === false ? 'failed' : '',
+          )}
+        >
+          <h2 className="result-bar--title">FAILED</h2>
+          <div className="result-bar--desc">
+            <p className="main">시간초과입니다</p>
+            <p className="">안타깝네요 😭</p>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+ResultBackground.defaultProps = {
+  success: true,
+};
+
+export default ResultBackground;
