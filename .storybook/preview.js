8631e8b0bfcb11b421565ec0ac52be0154bb9c4b
jeongnaehyeok <cnh0301@gmail.com>
2020-12-07 15:49:24 +0900

    [ Docs ] Storybook viewport 지정

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,42 @@
 import React from 'react';
 import '../src/assets/scss/project.scss';
 import '../src/assets/scss/storybook.scss';
+import { addParameters } from '@storybook/react';
+
+addParameters({
+  viewport: {
+    viewports: {
+      ExtraSmall: {
+        name: 'Iphone 5/SE',
+        styles: {
+          width: '320px',
+          height: '568px',
+        },
+      },
+      Small: {
+        name: 'Iphone 6/7/8',
+        styles: {
+          width: '375px',
+          height: '667px',
+        },
+      },
+      Medium: {
+        name: 'Iphone 7/8/9 Plus',
+        styles: {
+          width: '414px',
+          height: '736px',
+        },
+      },
+      Large: {
+        name: 'Ipad',
+        styles: {
+          width: '768px',
+          height: '1024px',
+        },
+      },
+    },
+  },
+});
 
 export const parameters = {
   backgrounds: {
