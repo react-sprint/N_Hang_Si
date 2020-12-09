6885035dc5352f2a6f6bb2f62ab6e0c1eb7113d1
jeongnaehyeok <cnh0301@gmail.com>
2020-12-10 00:12:47 +0900

    [ Fix ] 파일 경로 관련 Issue

---
+++ src/components/commons/ResultBox.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import classNames from 'classnames';
+import '@/assets/scss/commons/ResultBox.scss';
+
+const ResultBox = ({ title, text, color }) => {
+  return (
+    <div className={classNames('resultbox', color)}>
+      <span>{title}</span>
+      <span>{text}</span>
+    </div>
+  );
+};
+
+export default ResultBox;
+
+ResultBox.defaultProps = {
+  title: '주제어',
+  text: '7자까지 쓸게요',
+  color: 'black',
+};
