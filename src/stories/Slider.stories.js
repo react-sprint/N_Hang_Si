885cd80789aa4d4d943902e69ee969d823906373
Jiwon Jeon <ajiop0123@gmail.com>
2020-12-09 23:25:34 +0900

    [ Docs ] Slider Storybook 수정

--- src/stories/Slider.stories.js
+++ src/stories/Slider.stories.js
@@ -4,6 +4,20 @@ import Slider from '@/components/layouts/ingame/Slider';
 export default {
   title: 'Slider',
   component: Slider,
+  argTypes: {
+    topic: {
+      control: 'text',
+    },
+    indexNow: {
+      control: {
+        type: 'range',
+        default: 1,
+        max: 6,
+      },
+    },
+  },
 };
 
-export const Default = () => <Slider />;
+export const Default = ({ topic, indexNow }) => (
+  <Slider topic={topic} indexNow={indexNow} />
+);
