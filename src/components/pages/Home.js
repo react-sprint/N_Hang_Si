b35070001b1a0abf79a4402efa7a52fb013b8a55
Jiwon Jeon <ajiop0123@gmail.com>
2020-12-13 20:17:25 +0900

    [ Feature ] Home 페이지 추가

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import '@/assets/scss/pages/Home.scss';
+import Button from '@/components/commons/Button';
+import { useHistory, Link } from 'react-router-dom';
+import bannerImage from '@/assets/images/banner.png';
 
 const Home = () => {
-  return <div>Home</div>;
+  const history = useHistory();
+  const reDirect = () => {
+    history.push('/gameprepare');
+  };
+  return (
+    <div className="home">
+      <div className="home--banner">
+        <img src={bannerImage} alt="배너 이미지" />
+      </div>
+      <div className="home--button">
+        <Button contents="시작하기" color="orange" hook={reDirect} />
+        <Link to="/rule">게임 방법</Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
